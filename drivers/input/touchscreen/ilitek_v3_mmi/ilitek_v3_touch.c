cyberknight777/dragonheart_kernel_motorola_cancunf

ilitek_v3_mmi: Implement single tap detection
- This implementation utilizes a boolean called single_tap_enabled which will be set by MotoActions to enable tap to wake.
- The single_tap_enabled boolean is then utilized to register or to not register the input keycode for single click.
- The single_tap_enabled boolean is then exposed to userspace by a sysfs device attribute.
- Additionally, since this code is so convoluted, make it so that gc->type and key_code are set only if double_tap_enabled or single_tap_enabled whilst gc->code being the same as GESTURE_SINGLECLICK or GESTURE_DOUBLECLICK.
- Also skip the switch case if the checks explained above fails.

Change-Id: I509111bfacc40435e2b5857282302e1ea8ec98bd
Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

drivers/input/touchscreen/ilitek_v3_mmi/ilitek_v3_touch.c
@@ -2102,10 +2102,14 @@ void ili_report_gesture_mode(u8 *buf, int len)
 			return;
 		}
 		if (ilits->report_gesture_key) {
-			int key_code = KEY_F1;
+			int key_code;
 #ifdef ILI_DOUBLE_TAP_CTRL
 			if (GESTURE_DOUBLECLICK == gc->code && ilits->double_tap_enabled) {
 				key_code = KEY_WAKEUP;
+			} else if (GESTURE_SINGLECLICK == gc->code && ilits->single_tap_enabled) {
+				key_code = KEY_F1;
+			} else {
+				break;
 			}
 #endif
 			input_report_key(ilits->sensor_pdata->input_sensor_dev, key_code, 1);
@@ -2134,7 +2138,11 @@ void ili_report_gesture_mode(u8 *buf, int len)
 		input_report_key(input, KEY_GESTURE_POWER, 0);
 		input_sync(input);
 #endif
-		gc->type  = GESTURE_DOUBLECLICK;
+		if (GESTURE_DOUBLECLICK == gc->code && ilits->double_tap_enabled)
+			gc->type  = GESTURE_DOUBLECLICK;
+		else if (GESTURE_SINGLECLICK == gc->code && ilits->single_tap_enabled)
+			gc->type  = GESTURE_SINGLECLICK;
+
 		gc->clockwise = 1;
 		gc->pos_end.x = gc->pos_start.x;
 		gc->pos_end.y = gc->pos_start.y;

Fogos-Playground/android_kernel_motorola_sm6375

drivers/input/touchscreen/ilitek_v3_mmi/ilitek_v3_touch.c
@@ -2102,10 +2102,14 @@ void ili_report_gesture_mode(u8 *buf, int len)
 			return;
 		}
 		if (ilits->report_gesture_key) {
-			int key_code = BTN_TRIGGER_HAPPY3;
+			int key_code;
 #ifdef ILI_DOUBLE_TAP_CTRL
 			if (GESTURE_DOUBLECLICK == gc->code && ilits->double_tap_enabled) {
 				key_code = KEY_WAKEUP;
+			} else if (GESTURE_SINGLECLICK == gc->code && ilits->single_tap_enabled) {
+				key_code = BTN_TRIGGER_HAPPY3;
+			} else {
+				break;
 			}
 #endif
 			input_report_key(ilits->sensor_pdata->input_sensor_dev, key_code, 1);
@@ -2134,7 +2138,11 @@ void ili_report_gesture_mode(u8 *buf, int len)
 		input_report_key(input, KEY_GESTURE_POWER, 0);
 		input_sync(input);
 #endif
-		gc->type  = GESTURE_DOUBLECLICK;
+		if (GESTURE_DOUBLECLICK == gc->code && ilits->double_tap_enabled)
+			gc->type  = GESTURE_DOUBLECLICK;
+		else if (GESTURE_SINGLECLICK == gc->code && ilits->single_tap_enabled)
+			gc->type  = GESTURE_SINGLECLICK;
+
 		gc->clockwise = 1;
 		gc->pos_end.x = gc->pos_start.x;
 		gc->pos_end.y = gc->pos_start.y;